c983e3ed8398fd4a477e79017b35c5f9763e7067
PravinChoudhary11 <itspracin750@gmail.com>
2025-09-23 18:46:53 +0530

    chore: Update New Navigation System Introduced
    chore: bump Uni-UI to version 3.5

--- src/app/_components/InitialAppLoader.jsx
+++ src/app/_components/InitialAppLoader.jsx
@@ -297,6 +297,7 @@ const InitialAppLoader = () => {
           50%, 100% { transform: translateX(100%); }
         }
         
+        
         @keyframes shimmer {
           0% { transform: translateX(-100%); }
           100% { transform: translateX(100%); }
